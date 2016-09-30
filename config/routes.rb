73a1fc190a5f700e0216caeea7ae5d21aefac9c6
Abraham lagat <abraham.lagat.k@gmail.com>
2016-09-30 11:47:29 +0000

    add gem devise for accounts management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root 'pages#index'
+
+  get '/profile'=>'pages#profile'
+
+  get '/explore'=>'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
